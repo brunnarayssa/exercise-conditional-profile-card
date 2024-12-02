72689277119774b2fda044388f3a85551fc15c88
Brunna Carvalho <114610031+brunnarayssa@users.noreply.github.com>
2024-12-02 19:42:30 +0000

    renderizandocodigo

--- src/js/app.js
+++ src/js/app.js
@@ -3,47 +3,58 @@ import "../style/index.css";
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
  */
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+
+  // Determine cover image
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
+  // Construct full name
+  const fullName = `${variables.name || "Lucy"} ${variables.lastName ||
+    "Boilett"}`;
+
+  // Determine location
+  const location = `${variables.city || "Miami"}, ${variables.country ||
+    "USA"}`;
+
+  // Social media links
+  const socialMedia = {
+    twitter: variables.twitter
+      ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
+      : "",
+    github: variables.github
+      ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
+      : "",
+    linkedin: variables.linkedin
+      ? `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
+      : "",
+    instagram: variables.instagram
+      ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
+      : ""
+  };
+
+  // Social media position
+  const socialMediaClass =
+    variables.socialMediaPosition === "left"
+      ? "position-left"
+      : "position-right";
+
+  // Reset the website body with the new HTML output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${fullName}</h1>
+          <h2>${variables.role || "Web Developer"}</h2>
+          <h3>${location}</h3>
+          <ul class="${socialMediaClass}">
+            ${socialMedia.twitter}
+            ${socialMedia.github}
+            ${socialMedia.linkedin}
+            ${socialMedia.instagram}
           </ul>
-        </div>
-    `;
+        </div>`;
 }
 
 /**
@@ -58,7 +69,7 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "right",
     // social media usernames
     twitter: null,
     github: null,
