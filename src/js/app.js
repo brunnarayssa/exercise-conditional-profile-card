5bff0d6aa6f4dfb0e3c1bdc9f5c6b88851f0199f
Brunna Carvalho <114610031+brunnarayssa@users.noreply.github.com>
2024-12-04 19:27:12 +0000

    renderizandoclassfilters

--- src/js/app.js
+++ src/js/app.js
@@ -1,91 +1,95 @@
 import "../style/index.css";
 
+/**
+ *  EDIT ONLY INSIDE THIS RENDER FUNCTION
+ *  This function is called every time the user changes types or changes any input
+ * 
+    {
+        includeCover: true, // if includeCover is true the algorithm should show the cover image
+        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
+        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+        socialMediaPosition: "right", // social media bar position (left or right)
+        
+        twitter: null, // social media usernames
+        github: null,
+        linkedin: null,
+        instagram: null,
+
+        name: null,
+        lastName: null,
+        role: null,
+        country: null,
+        city: null
+    }
+ */
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
-
-  // Determina la imagen de fondo
+  // here we ask the logical questions to make decisions on how to build the html
+  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover === false) cover = "<div class='cover'></div>";
+  if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // Construye el nombre completo
-  const fullName = `${variables.name || "Lucy"} ${variables.lastName ||
-    "Boilett"}`;
-
-  // Determina la ubicación
-  const location = `${variables.city || "Miami"}, ${variables.country ||
-    "USA"}`;
-
-  // Enlaces de redes sociales
-  const socialMedia = {
-    twitter: variables.twitter
-      ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
-      : "",
-    github: variables.github
-      ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
-      : "",
-    linkedin: variables.linkedin
-      ? `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
-      : "",
-    instagram: variables.instagram
-      ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
-      : ""
-  };
-
-  // Determina la clase de posición de las redes sociales
-  const socialMediaClass =
-    variables.socialMediaPosition === "position-left"
-      ? "position-left"
-      : "position-right";
-
-  // Reinicia el contenido HTML de la página con la nueva salida HTML
+  // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${fullName}</h1>
-          <h2>${variables.role || "Web Developer"}</h2>
-          <h3>${location}</h3>
-          <ul class="${socialMediaClass}">
-            ${socialMedia.twitter}
-            ${socialMedia.github}
-            ${socialMedia.linkedin}
-            ${socialMedia.instagram}
+          <h1>${variables.name || ""} ${variables.lastName || ""} </h1>
+          <h2>${variables.role || ""}</h2>
+          <h3>${variables.city || ""}, ${variables.country || ""} </h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${variables.twitter ||
+              ""}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${variables.github ||
+              ""}"><i class="fab fa-github"></i></a></li>
+            <li><a href="${variables.linkedin ||
+              ""}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${variables.instagram ||
+              ""}"><i class="fab fa-instagram"></i></a></li>
           </ul>
-        </div>`;
+        </div>
+    `;
 }
 
-// Cuando la página carga
+/**
+ * Don't change any of the lines below, here is where we do the logic for the dropdowns
+ */
 window.onload = function() {
   window.variables = {
+    // if includeCover is true the algorithm should show the cover image
     includeCover: true,
+    // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    socialMediaPosition: "position-right", // Cambia entre "position-left" y "position-right"
-    twitter: "lucy_twitter",
-    github: "lucy_github",
-    linkedin: "lucy_linkedin",
-    instagram: "lucy_instagram",
-    name: "Lucy",
-    lastName: "Boilett",
-    role: "Web Developer",
-    country: "USA",
-    city: "Miami"
+    // social media bar position (left or right)
+    socialMediaPosition: "position-left",
+    // social media usernames
+    twitter: null,
+    github: null,
+    linkedin: null,
+    instagram: null,
+    name: null,
+    lastName: null,
+    role: null,
+    country: null,
+    city: null
   };
-  render(window.variables);
+  render(window.variables); // render the card for the first time
 
-  // Escucha los cambios en los inputs
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
-      const attribute = e.target.getAttribute("for");
+      // <- add a listener to every input
+      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
       let values = {};
       values[attribute] =
-        this.value === "" || this.value === "null"
+        this.value == "" || this.value == "null"
           ? null
-          : this.value === "true"
+          : this.value == "true"
           ? true
-          : this.value === "false"
+          : this.value == "false"
           ? false
           : this.value;
-      render(Object.assign(window.variables, values)); // Rerender con nuevos valores
+      render(Object.assign(window.variables, values)); // render again the card with new values
     });
   });
 };
